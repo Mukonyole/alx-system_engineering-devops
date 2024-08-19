5c287bd9582d885212a89358083dde902660f7b5
Evans Mukonyole <emukonyole@gmail.com>
2024-08-19 07:48:21 -0700

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# It change OS configuration to make it possible to login with the
+# holberton user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
